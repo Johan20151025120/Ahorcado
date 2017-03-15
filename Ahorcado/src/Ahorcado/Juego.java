ac0321f5a3bb54f4e29df896c2f639c6b4832926
Estudiantes <Estudiantes@Labing50618.udistrital.edu.co>
2017-03-15 16:56:22 -0500

    Todo

---
+++ Ahorcado/src/Ahorcado/Juego.java
@@ -0,0 +1,31 @@
+package Ahorcado;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Juego {
+    
+    
+    char PalabraJugador;
+    Palabras mipalabra = new Palabras();
+    
+    String txt = Palabras.getText();
+    void llenado (char[] letra) {
+    
+       for (int i = 0; i < 10; i++) {
+           for (int j=1; j<1; j++){
+                char[] palabra = new char[10]; 
+                
+                       
+        }
+       }
+    
+    }
+}
